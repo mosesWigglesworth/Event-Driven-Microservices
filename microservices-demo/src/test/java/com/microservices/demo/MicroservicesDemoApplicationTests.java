d6f0ed73188167bfb12b2df679598e3389a83fe9
mosesWigglesworth <moseswigglesworth@gmail.com>
2024-12-11 15:04:46 +0530

    Base Microservice Code - Recommit

--- microservices-demo/src/test/java/com/microservices/demo/MicroservicesDemoApplicationTests.java
+++ microservices-demo/src/test/java/com/microservices/demo/MicroservicesDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MicroservicesDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
